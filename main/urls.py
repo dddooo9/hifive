5a3776f40c75ab0ae80cc76fbe679f7ec3cd573d
Jy-do <jeeyulee98@naver.com>
2021-08-23 21:42:34 +0900

    exercise.html mypage.html 생성해서 홈 화면과 연결

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('mypage/', views.mypage, name="mypage"),
+    path('exercise/', views.exercise, name="exercise"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+def mypage(request):
+    return render(request, 'main/mypage.html')
+
+def exercise(request):
+    return render(request, 'main/exercise.html')
