db0dc2d09884b57b2798071566a58930f2d1a6f3
bgs-study <qkrwlgp@gmail.com>
2021-09-29 21:44:40 +0900

    developer,contact 관련 url,view 추가

--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+def contact(request):
+    return render(request, 'main/contact.html')
+def developer(request):
+    return render(request, 'main/developer.html')
