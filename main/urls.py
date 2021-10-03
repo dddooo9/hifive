0ae2a53c6114075808d3d2ab3d54a19d1511734d
bgs-study <qkrwlgp@gmail.com>
2021-10-03 16:41:00 +0900

    communicate페이지작업

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 app_name = 'main'
 urlpatterns = [
+    path('', views.index, name="index"),
     path('contact/', views.contact, name="contact"),
     path('developer/', views.developer, name="developer"),
-    path('', views.index, name="index"),
+    path('communicate/', views.paging, name="paging"),
+    path('communicate/<int:question_id>/', views.detail, name='detail'),
+    path('communicate/question_create/', views.question_create, name='question_create'),
+    path('communicate/answer_create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
